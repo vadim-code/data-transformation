585ac1705cf9b2b190fb2b6e2fe520bc031c0981
Vadim Konstantinov <vadim@MacBook-Pro-Vadim.local>
2022-08-26 23:47:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Test Task
+
+## Requirements
+
+We need to write a function to transform the data into a suitable form for the table
+
+## Job Reference
+
+`https://docs.google.com/document/d/1A6OfRwyw4MrfNepMF4z2M91nG32b4s2SpHqiJbk1eE0/edit`
+
+# Time Spent
+
+1.5 hours
---
+++ transformation.js
@@ -0,0 +1,275 @@
+const data = {
+  portal_qry_wq: [
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '1/16/2019',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2018-2019',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '1/16/2019',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2018-2019',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '12/15/2016',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2016-2017',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '12/15/2016',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2016-2017',
+      result_display: '1.5',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '12/30/2020',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Biochemical Oxygen Demand (BOD) (5-day @ 20 Deg. C)',
+      report_year: '2020-2021',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '12/30/2020',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2020-2021',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '12/30/2020',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'pH',
+      report_year: '2020-2021',
+      result_display: '7.1',
+      units: 'SU',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '12/30/2020',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2020-2021',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '1/25/2018',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2017-2018',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '1/25/2018',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2017-2018',
+      result_display: '0.9',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/10/2021',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Biochemical Oxygen Demand (BOD) (5-day @ 20 Deg. C)',
+      report_year: '2020-2021',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/10/2021',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2020-2021',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/10/2021',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'pH',
+      report_year: '2020-2021',
+      result_display: '7',
+      units: 'SU',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/10/2021',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2020-2021',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/15/2018',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2017-2018',
+      result_display: '7.3',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/15/2018',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2017-2018',
+      result_display: '1.7',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/27/2017',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2016-2017',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '3/27/2017',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2016-2017',
+      result_display: 'ND',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '5/15/2019',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2018-2019',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '5/15/2019',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'pH',
+      report_year: '2018-2019',
+      result_display: '7',
+      units: 'pH Units',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '5/15/2019',
+      monitoringpoint: 'Outfall 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2018-2019',
+      result_display: '5.6',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '5/18/2020',
+      monitoringpoint: 'OUTFALL 1',
+      parameter_smarts: 'Oil and Grease',
+      report_year: '2019-2020',
+      result_display: 'DNQ',
+      units: 'mg/L',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '5/18/2020',
+      monitoringpoint: 'OUTFALL 1',
+      parameter_smarts: 'pH',
+      report_year: '2019-2020',
+      result_display: '7',
+      units: 'SU',
+    },
+    {
+      __typename: 'portal_qry_wq',
+      collectiondate: '5/18/2020',
+      monitoringpoint: 'OUTFALL 1',
+      parameter_smarts: 'Total Suspended Solids (TSS)',
+      report_year: '2019-2020',
+      result_display: '1.8',
+      units: 'mg/L',
+    },
+  ],
+};
+
+const waterVariants = data['portal_qry_wq'];
+
+function appropriateDate(arr) {
+  const res = arr.reduce((acc, el) => {
+    if (!acc[el.collectiondate]) {
+      acc[el.collectiondate] = {
+        monitoringpoint: el.monitoringpoint,
+        oilAndGrease: '',
+        tss: '',
+        bod: '',
+        ph: '',
+      };
+    }
+
+    if (el.parameter_smarts === 'Oil and Grease') {
+      acc[el.collectiondate].oilAndGrease = el.result_display;
+    } else if (el.parameter_smarts === 'Total Suspended Solids (TSS)') {
+      acc[el.collectiondate].tss = el.result_display;
+    } else if (
+      el.parameter_smarts ===
+      'Biochemical Oxygen Demand (BOD) (5-day @ 20 Deg. C)'
+    ) {
+      acc[el.collectiondate].bod = el.result_display;
+    } else if (el.parameter_smarts === 'pH') {
+      acc[el.collectiondate].ph = el.result_display;
+    }
+
+    return acc;
+  }, {});
+
+  return Object.keys(res)
+    .map((date) => {
+      return {
+        date,
+        ...res[date],
+      };
+    })
+    .map((el) => {
+      return {
+        sampleInformations: {
+          date: el.date,
+          outfallIdentifications: el.monitoringpoint,
+        },
+        analiticalParameters: {
+          oilAndGrease: el.oilAndGrease,
+          totalSuspendedSolids: el.tss,
+          biochemicalOxygenDemand: el.bod,
+          pH: el.ph,
+        },
+      };
+    });
+}
+
+console.log('appropriateDate =>', appropriateDate(waterVariants));
